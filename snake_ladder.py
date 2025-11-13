eb7b684a6fb7eaac9623baa8e2bc116a11000ebe
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-13 16:55:12 +0500

    Update snake_ladder.py

--- snake_ladder.py
+++ snake_ladder.py
@@ -16,7 +16,7 @@ com_started = False
 turn = 0
 while True:
     turn += 1
-    print("\n------------------------------------")
+    print("\n-------------------------------------")
 
     
     if turn % 2 == 0:
@@ -112,6 +112,7 @@ while True:
                 if roll == 6:
                     print("🔥 Computer gets another turn!")
                     turn -= 1
+
 
 
 
